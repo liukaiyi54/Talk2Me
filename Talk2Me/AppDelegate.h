663460f5529e68933e2ee8418bac8a7f9e3aaa6d
Michael <liukaiyi54@gmail.com>
2015-11-17 14:45:08 +0800

    Initial comment

---
+++ Talk2Me/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Talk2Me
+//
+//  Created by Michael on 15/11/17.
+//  Copyright © 2015年 Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Talk2Me/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Talk2Me
+//
+//  Created by Michael on 15/11/17.
+//  Copyright © 2015年 Michael. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
