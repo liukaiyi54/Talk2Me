130e7025dd44ffd44686ff6c5d6439f96f2756c3
Michael <liukaiyi54@gmail.com>
2015-11-19 09:43:57 +0800

    新建MessageTextView

---
+++ Talk2Me/MessageTextView.m
@@ -0,0 +1,13 @@
+//
+//  MessageTextView.m
+//  Talk2Me
+//
+//  Created by Michael on 15/11/18.
+//  Copyright © 2015年 Michael. All rights reserved.
+//
+
+#import "MessageTextView.h"
+
+@implementation MessageTextView
+
+@end
