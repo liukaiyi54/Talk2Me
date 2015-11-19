338f0fb0f8df9026c42be41308d3bbacbc2a2a6d
Michael <liukaiyi54@gmail.com>
2015-11-19 09:43:37 +0800

    新建Message Model

---
+++ Talk2Me/Message.m
@@ -0,0 +1,13 @@
+//
+//  Message.m
+//  Talk2Me
+//
+//  Created by Michael on 15/11/18.
+//  Copyright © 2015年 Michael. All rights reserved.
+//
+
+#import "Message.h"
+
+@implementation Message
+
+@end
