e551d880d538a18d79e48e11b4ceefd758d935f2
Michael <liukaiyi54@gmail.com>
2015-11-20 09:38:22 +0800

    完成TableView的设计

--- Talk2Me/ViewController.m
+++ Talk2Me/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 #import "Message.h"
 #import "MessageTextView.h"
+#import "MessageTableViewCell.h"
+
+static NSString *MessengerCellIdentifier = @"MessengerCell";
+static NSString *AutoCompletionCellIdentifier = @"AutoCompletionCell";
 
 @interface ViewController ()
 
@@ -41,31 +45,144 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.textView.keyboardType = UIKeyboardTypeDefault;
+    
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    
     NSMutableArray *array = [NSMutableArray new];
-    for (NSInteger i = 0; i < 10; i++) {
-        Message *message = [Message new];
-        message.userName = @"Michael";
-        message.text = @"don't you miss me, i know you do";
-        [array addObject:message];
-    }
     
-    NSArray *reversed = [[array reverseObjectEnumerator] allObjects];
+//    NSArray *reversed = [[array reverseObjectEnumerator] allObjects];
     
-    self.messages = [[NSMutableArray alloc] initWithArray:reversed];
+    self.messages = [[NSMutableArray alloc] initWithArray:array];
     
     self.shakeToClearEnabled = YES;
-
-//    self.textInputbar.maxCharCount = 256;
-//    self.textInputbar.counterStyle = SLKCounterStyleSplit;
-//    self.textInputbar.counterPosition = SLKCounterPositionTop;
     
     self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
-    
+    [self.tableView registerClass:[MessageTableViewCell class] forCellReuseIdentifier:MessengerCellIdentifier];
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
 
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
+
+#pragma mark - overriden Methods
+- (void)didPressRightButton:(id)sender {
+    [self.textView refreshFirstResponder];
+    
+    Message *message = [Message new];
+    message.userName = @"Mic";
+    message.text = [self.textView.text copy];
+    
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
+    
+    UITableViewRowAnimation rowAnimation = self.inverted ? UITableViewRowAnimationBottom : UITableViewRowAnimationTop;
+    UITableViewScrollPosition scrollPosition = self.inverted ? UITableViewScrollPositionBottom : UITableViewScrollPositionTop;
+    
+    [self.tableView beginUpdates];
+    [self.messages insertObject:message atIndex:0];
+    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:rowAnimation];
+    [self.tableView endUpdates];
+    
+    [self.tableView scrollToRowAtIndexPath:indexPath atScrollPosition:scrollPosition animated:YES];
+    
+    [self.tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+    
+    [super didPressRightButton:sender];
+}
+
+- (NSString *)keyForTextCaching {
+    return [[NSBundle mainBundle] bundleIdentifier];
+}
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.messages.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return [self messageCellForRowAtIndexPath:indexPath];
+}
+
+- (MessageTableViewCell *)messageCellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    MessageTableViewCell *cell = (MessageTableViewCell *)[self.tableView dequeueReusableCellWithIdentifier:MessengerCellIdentifier];
+    
+    if (!cell.textLabel.text) {
+        UILongPressGestureRecognizer *longPress = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(didLongPressCell:)];
+        [cell addGestureRecognizer:longPress];
+    }
+    
+    Message *message = self.messages[indexPath.row];
+    
+    cell.titleLabel.text = message.userName;
+    cell.bodyLabel.text = message.text;
+    
+    cell.indexPath = indexPath;
+
+    cell.transform = self.tableView.transform;
+    
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    Message *message = self.messages[indexPath.row];
+    
+    NSMutableParagraphStyle *paragraphStyle = [NSMutableParagraphStyle new];
+    paragraphStyle.lineBreakMode = NSLineBreakByCharWrapping;
+    paragraphStyle.alignment = NSTextAlignmentLeft;
+    
+    CGFloat pointSize = [MessageTableViewCell defaultFontSize];
+    
+    NSDictionary *attributes = @{NSFontAttributeName: [UIFont systemFontOfSize:pointSize],
+                                 NSParagraphStyleAttributeName: paragraphStyle};
+    
+    CGFloat width = CGRectGetWidth(tableView.frame)-kMessageTableViewCellAvatarHeight;
+    
+    CGRect titleBounds = [message.userName boundingRectWithSize:CGSizeMake(width, CGFLOAT_MAX) options:NSStringDrawingUsesLineFragmentOrigin attributes:attributes context:NULL];
+    CGRect bodyBounds = [message.text boundingRectWithSize:CGSizeMake(width, CGFLOAT_MAX) options:NSStringDrawingUsesLineFragmentOrigin attributes:attributes context:NULL];
+    
+    if (message.text.length == 0) {
+        return 0.0;
+    }
+    
+    CGFloat height = CGRectGetHeight(titleBounds);
+    height += CGRectGetHeight(bodyBounds);
+    height += 40.0;
+    
+    if (height < kMessageTableViewCellMinimumHeight) {
+        height = kMessageTableViewCellMinimumHeight;
+    }
+    
+    return height;
+}
+
+#pragma mark - event handlers
 - (void)commonInit {
     [[NSNotificationCenter defaultCenter] addObserver:self.tableView selector:@selector(reloadData) name:UIContentSizeCategoryDidChangeNotification object:nil];
     [self registerClassForTextView:[MessageTextView class]];
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @end
